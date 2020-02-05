8c2e69cfd52b3edec2cfa82192e5c82d830bc1d0
Babak Esfandiari <babak@sce.carleton.ca>
2020-02-05 02:50:00 -0500

    added tests, added .travis.yml file

---
+++ src/test/java/babak/addressbook2020/addressbook/BuddyWebLayerTest.java
@@ -0,0 +1,29 @@
+package babak.addressbook2020.addressbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BuddyWebLayerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/welcome")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello Buddy!")));
+    }
+}
